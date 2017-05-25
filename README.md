e7450fbef7e1f29985ee9ee45736ca78f9e7e6a7
CalcCalcCalc <alecdsstone@gmail.com>
2017-05-25 16:03:46 +0100

    toPrecision() used to truncate stage and answer values

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# material-calculator
+Material calculator, Created by Alec Stone as part of the Free Code Camp Front-End development syllabus.
--- js/index.js
+++ js/index.js
@@ -1,73 +1,80 @@
-$(document).ready(function (){
-	var stage = [];
-	var answer = [];
-	var isAnswer = false;
-	var operatorActive = false;
-	var decimalFlag = false;
+$(document).ready(function() {
+  var stage = [];
+  var answer = [];
+  var isAnswer = false;
+  var operatorActive = false;
+  var decimalFlag = false;
 
-	function ResetCalc(){
-		stage = [];
-		answer = [];
-		$("#stage").html("<p> </p>");
-		$("#answer").html("<p> </p>");
-		isAnswer = false;
-		operatorActive = true;
-		decimalFlag = false;
-	}
+  function ResetCalc() {
+    stage = [];
+    answer = [];
+    $("#stage").html("<p> </p>");
+    $("#answer").html("<p> </p>");
+    isAnswer = false;
+    operatorActive = true;
+    decimalFlag = false;
+  }
 
-	$("button").click(function(){
-		switch(this.value){
-			case "C": // clears all variables and arrays
-				ResetCalc();
-				return;
-			case '=': // replaces stage with answer, then clears answer
-				stage = [answer];
-				answer = [];
-				$("#answer").html("<p> </p>");
-				$("#stage").html("<p>" + stage + "</p>");
-				operatorActive = false;
-				isAnswer = true;
-				decimalFlag = false;
-				return;
-			default: // an evaluable button is pressed
-			// if operator active, replace operator
-				if (this.value == "+" || this.value == "-"|| this.value == "*"|| this.value == "/"){
-					if (operatorActive){
-						var splitStage = stage[0].split("");
-						splitStage.pop();
-						stage = [splitStage.join("")];
-					}
-					operatorActive = true;
-					isAnswer = false;
-				}else{
-					operatorActive = false;
-					if (isAnswer){
-						stage = [];
-						isAnswer = false;
-					}
-				}
+  $("button").click(function() {
+    switch (this.value) {
+      case "C": // clears all variables and arrays
+        ResetCalc();
+        return;
+      case '=': // replaces stage with answer, then clears answer
+        stage = [answer];
+        answer = [];
+        $("#answer").html("<p> </p>");
+        $("#stage").html("<p>" + stage + "</p>");
+        operatorActive = false;
+        isAnswer = true;
+        decimalFlag = false;
+        return;
+      default: // an evaluable button is pressed
+        // if operator active, replace operator
+        if (this.value == "+" || this.value == "-" || this.value == "*" || this.value == "/") {
+          if (operatorActive) {
+            var splitStage = stage[0].split("");
+            splitStage.pop();
+            stage = [splitStage.join("")];
+          }
+          operatorActive = true;
+          isAnswer = false;
+        } else {
+          operatorActive = false;
+          if (isAnswer) {
+            stage = [];
+            isAnswer = false;
+          }
+        }
 
-				if (operatorActive){
-					decimalFlag = false;
-				}
+        if (operatorActive) {
+          decimalFlag = false;
+        }
 
-				if (this.value == "."){ // todo: no decimal point allowed after operator
-					if (decimalFlag){
-						return;
-					}
-					decimalFlag = true;
-				}
+        if (this.value == ".") { // todo: no decimal point allowed after operator
+          if (decimalFlag) {
+            return;
+          }
+          decimalFlag = true;
+        }
 
 
-				stage.push(this.value);
-				stage = [stage.join("")];
-			}
-			if (!operatorActive){
-				answer = eval(stage.join(""));
-				$("#answer").html("<p>" + answer + "</p>");
-			}
+        stage.push(this.value);
+        stage = [stage.join("")];
+    }
+		var splitStage = stage[0].split("");
+		if (splitStage.length > 16){
+			var num = parseFloat(splitStage.join("")).toPrecision(8);
+			stage = [num];
+		}
 
-		$("#stage").html("<p>" + stage + "</p>");
 
-	});
+    if (!operatorActive) {
+      answer = eval(stage.join(""));
+      $("#answer").html("<p>" + answer + "</p>");
+    }
+
+    $("#stage").html("<p>" + stage + "</p>");
+
+  });
 });
